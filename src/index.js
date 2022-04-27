c0c1efe913c7334ab79187639d0bd50556fd678e
Nam Nguyen <nam.nguyen.node@gmail.com>
2022-04-27 07:57:18 +0700

    🐬

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// import App from "./App";
-import App from "./components/TagsInput";
+import App from "./App";
+// import App from "./components/TagsInput";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
